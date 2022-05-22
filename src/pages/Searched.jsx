6cc015e27198c66323386622193f43aa85d3d468
Marek <531devv@gmail.com>
2022-05-22 17:45:00 +0200

    Adding routes and single recipe page

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,30 +1,33 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 function Searched() {
   const [search, setSearch] = useState([]);
   const params = useParams();
 
-  const getSearched = async (search) => {
+  const getSearched = async (name) => {
     const data = await fetch(
-      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&query${search}&number=9`
+      `https://api.spoonacular.com/recipes/complexSearch?query=${name}&apiKey=${process.env.REACT_APP_API_KEY}&number=9`
     );
     const recipes = await data.json();
     setSearch(recipes.results);
   };
 
   useEffect(() => {
-    getSearched(search);
+    getSearched(params.search);
   }, [params.search]);
 
   return (
     <Grid>
       {search.map((item) => {
         return (
-          <Card key={item.key}>
-            <img src={item.image} alt={item.title} />
-            <h4>{item.image}</h4>
+          <Card key={item.id}>
+            <Link to={"/recipe/" + item.id}>
+              <img src={item.image} alt={item.title} />
+              <h4>{item.title}</h4>
+            </Link>
           </Card>
         );
       })}
